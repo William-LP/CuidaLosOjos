20d95f38bd9e468593cdd68d5231cce11bcc9f83
William-LP <william.lepommelet@gmail.com>
2020-06-24 10:21:02 +0200

    Add project files.

---
+++ CuidaLosOjos/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CuidaLosOjos
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerBox = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timerBox
+            // 
+            this.timerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerBox.Location = new System.Drawing.Point(12, 12);
+            this.timerBox.Name = "timerBox";
+            this.timerBox.Size = new System.Drawing.Size(105, 45);
+            this.timerBox.TabIndex = 0;
+            this.timerBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.timerBox.Click += new System.EventHandler(this.timerBox_Click);
+            this.timerBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timerBox_KeyPress);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(12, 63);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(105, 41);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(127, 116);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.timerBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Cuida Los Ojos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox timerBox;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ CuidaLosOjos/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuidaLosOjos
+{
+    public partial class mainForm : Form
+    {
+
+        private int timeLeft;
+        private int interval;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            timerBox.Text = "0";
+           
+            
+        }
+
+        private void timerBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) {
+                e.Handled = true;
+           } 
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+            if (startBtn.Text == "START")
+            {
+                if (timerBox.Text == "0")
+                {
+                    MessageBox.Show("Combien de minutes entre chaque alertes ?", "Saisie un interval de temps", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    startBtn.Text = "STOP";                  
+                    interval = Convert.ToInt32(timerBox.Text);
+                    timeLeft = (interval * 60);
+
+                   
+                    timerBox.ReadOnly = true;
+
+                    timer.Tick += new EventHandler(timer_Tick);
+                    timer.Start();
+                }
+            } else
+            {
+                startBtn.Text = "START";
+                timer.Stop();
+                timeLeft = 0;
+                timerBox.ReadOnly = false;
+                timerBox.Text = "0";
+            }
+
+
+
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {            
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                var timespan = TimeSpan.FromSeconds(timeLeft);
+                int minutesLeft = (timeLeft / 60) + 1;
+                timerBox.Text = minutesLeft.ToString();
+            }
+            else
+            {
+                timer.Stop();                
+                System.Windows.Forms.MessageBox.Show("Regarde au loin pendant 2 minutes !", "Cuida Los Ojos Querida !", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                timerBox.Text = interval.ToString();
+                timeLeft = (interval * 60);
+                timer.Start();
+
+
+
+            }
+        }
+
+        private void timerBox_Click(object sender, EventArgs e)
+        {
+            timerBox.Text = "";
+        }
+    }
+}
